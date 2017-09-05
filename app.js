967151392307d2413839ce60b1e8acc1a4ace1be
Brown <b9602104@gmail.com>
2017-09-05 19:30:45 +0800

    First_Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * @brief 使用 Nodejs 建立簡單 Web 應用程式
+ * 
+ * ＠author : Brown Huang
+ */
+
+//引用 express 套件
+const express = require('express')
+
+//取得環境變數的 Port  
+const PORT = process.env.PORT || 3000
+
+//建立 express 物件
+const app = express()
+
+//路徑 (ex : 127.0.0.1:3000/)
+app.get('/', function(req, res){
+    res.send('Hello World')
+})
+
+//Web 監聽的 Port 
+app.listen(PORT, function(){
+    console.log('Example app listening on port 3000!')
+})
